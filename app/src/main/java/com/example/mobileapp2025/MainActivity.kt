382465884909efd49294b90317b267162137b0e5
SweetBravery <camoru1051@gmail.com>
2025-07-23 14:11:33 -0400

    error de Linux

--- app/src/main/java/com/example/mobileapp2025/MainActivity.kt
+++ app/src/main/java/com/example/mobileapp2025/MainActivity.kt
@@ -1,16 +1,36 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.mobileapp2025
 
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.common.MediaItem
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Pause
+import androidx.compose.material.icons.filled.SkipNext
+import androidx.compose.material.icons.filled.SkipPrevious
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
 import com.example.mobileapp2025.ui.theme.MobileApp2025Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +39,69 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MobileApp2025Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun rememberPlayer(context: Context, uri:Uri): ExoPlayer {
+    val player = remember{
+        ExoPlayer.Builder(context).build().apply {
+            val mediaItem = MediaItem.fromUri(uri)
+            setMediaItem(mediaItem)
+            prepare()
+        }
+    }
+    DisposableEffect(Unit) {
+        onDispose {
+            player.release()
+        }
+    }
+    return player
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MobileApp2025Theme {
-        Greeting("Android")
+fun MediaControlBar(onPlay: ()->Unit, onPause: ()->Unit) {
+    var isPlaying by remember { mutableStateOf(false) }
+    //Boton de atras
+    IconButton(onClick = {}) {
+        Icon(imageVector = Icons.Filled.SkipPrevious, contentDescription = "Anterior")
+    }
+    //Boton de Pausar/Reproducir intercambiable
+    IconButton(onClick = {
+        if (isPlaying) {onPause()}
+        else {onPlay()}
+        isPlaying = !isPlaying
+    }) {
+        Icon(imageVector = if (isPlaying) Icons.Default.Pause else Icons.Default.PlayArrow,
+            contentDescription = if (isPlaying) "Pausar" else "Reproducir")
+    }
+    //Boton de Adelante
+    IconButton(onClick = {}) {
+        Icon(imageVector = Icons.Filled.SkipNext, contentDescription = "Posterior")
+    }
+}
+
+@Composable
+fun MainScreen() {
+    val context = LocalContext.current
+    val uri = "placeholderuri"
+    val player = rememberPlayer(context, uri)
+
+    Scaffold (
+        topBar = {
+            TopAppBar(title = { Text("Music PLayer") });
+        }
+        bottomBar = {
+            MediaControlBar(onPlay = player.play(), onPause = player.pause())
+            )
+        }
+    )
+    {
+        padding -> Column(modifier = Modifier.padding(padding)) {
+            PlayerScreen(player = player)
+        }
     }
 }
